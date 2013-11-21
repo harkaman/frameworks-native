aaa28a848081c1b06b48dee323003bf2b80e5a32
Praveena Pachipulusu <pveena@codeaurora.org>
2013-11-21 18:57:31 +0530

    SurfaceFlinger: Add NULL check for buffer handling
    Add buffer handling NULL check in dequeueBuffer and
    verify whether the output data from binder is not NULL
    in queueBuffer and connect api's to avoid SF crash
    
    CRs-Fixed: 573088
    CRs-Fixed: 572315
    
    Change-Id: I41cebbc0cbcbbb0fd5ecb38db7ec7b0c91cdffe9

--- libs/gui/IGraphicBufferProducer.cpp
+++ libs/gui/IGraphicBufferProducer.cpp
@@ -205,7 +205,11 @@ public:
         if (result != NO_ERROR) {
             return result;
         }
-        memcpy(output, reply.readInplace(sizeof(*output)), sizeof(*output));
+        if (const void *out_data = reply.readInplace(sizeof(*output))) {
+            memcpy(output, out_data, sizeof(*output));
+        } else {
+            return BAD_VALUE;
+        }
         result = reply.readInt32();
         return result;
     }
@@ -252,7 +256,11 @@ public:
         if (result != NO_ERROR) {
             return result;
         }
-        memcpy(output, reply.readInplace(sizeof(*output)), sizeof(*output));
+        if (const void *out_data = reply.readInplace(sizeof(*output))) {
+            memcpy(output, out_data, sizeof(*output));
+        } else {
+            return BAD_VALUE;
+        }
         result = reply.readInt32();
         return result;
     }
